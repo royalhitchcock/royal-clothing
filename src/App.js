d7731787da899910e6b8ce7b89321753b94caf86
that guy <trey.hitchcock@gmail.com>
2022-03-08 16:50:56 -0800

    update our menu-items to navigate with withRoute

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,7 +14,7 @@ class Directory extends Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -43,16 +43,14 @@ class Directory extends Component {
                   linkUrl: 'shop/mens'
                 }
               ]
-        }
+        };
     }
     render() {
         return (
             <div className='directory-menu'>
-                {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl}/>
-                        ))
-                }
+                {this.state.sections.map(({id, ...otherSectionProps }) => (
+                    <MenuItem key={id} { ...otherSectionProps }/>
+                ))}
             </div>
         )
     }
